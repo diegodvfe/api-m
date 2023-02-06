35e4c61e0d8ac98e3530f5eb53f85f8d0bc10a4e
iscodevdt <blesskaffeegt@gmail.com>
2023-02-06 16:37:12 -0600

    fixed a bug in output placehholder file

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const replaceTemplate = (temp, product) => {
     output = output.replace(/{%FROM%}/g, product.from)
     otuput = output.replace(/{%NUTRITIONS%}/g, product.nutrients)
     output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    otuput = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%PRICE%}/g, product.price)
     output = output.replace(/{%DESCRIPTION%}/g, product.description)
     
     if (!product.organic) {
