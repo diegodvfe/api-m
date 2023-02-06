86b137537da4becd2af8f23e56abe41113bc951a
iscodevdt <blesskaffeegt@gmail.com>
2023-02-06 16:22:11 -0600

    adding the templates dynamic

--- index.js
+++ index.js
@@ -36,7 +36,23 @@ const url = require('url')
 
 // Replace the placehoder objects
 const replaceTemplate = (temp, product) => {
-    // creo una variable dentro del 
+    // creo una variable dentro del
+
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%ID%}/g, product.id)
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from)
+    otuput = output.replace(/{%NUTRITIONS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    otuput = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    }
+
+    return output
+
 }
 
 // async version 
@@ -56,8 +72,9 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-type': 'text/html' })
 
     //realizamos en variable hacemos uso de dataObj.map para pa
-        const cardHtml = dataObj.map(el => replaceTemplate(templateCard, el))
-        res.end(templateOverview);
+        const cardHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('')
+        const output = templateOverview.replace('{%PRODUCTS_CARDS%}', cardHtml)
+        res.end(output);
         // res.end(templateCard)
     
 {/* Product page */}
